0db59ec65c90a3da0f4949328205eff17e39530e
Claudia <pinguiri1987@gmail.com>
2022-05-08 21:58:24 -0500

    fixing errors characters

--- components/Creativity.js
+++ components/Creativity.js
@@ -6,7 +6,7 @@ export const Creativity = () => {
   return (
     <div className={styles.creativityContainer}>
       <div className={styles.creativityPink}>
-        <h3>"Creativity is nothing but the way to solve new problems"</h3>
+        <h3>Creativity is nothing but the way to solve new problems</h3>
       </div>
       <div className={styles.creativityVolcan}>
         <Image src={"/volcan.jpg"} width={400} height={400} />
